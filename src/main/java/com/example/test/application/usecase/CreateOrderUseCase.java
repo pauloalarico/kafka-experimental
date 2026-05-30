e991c19e7fc43df16e02020aacffb230fe17bed6
pauloalarico <paulocraft20@gmail.com>
2026-05-30 05:06:57 -0300

    feat: adapt service to call the event publisher after processing the Order

--- src/main/java/com/example/test/application/usecase/CreateOrderUseCase.java
+++ src/main/java/com/example/test/application/usecase/CreateOrderUseCase.java
@@ -2,8 +2,10 @@ package com.example.test.application.usecase;
 
 import com.example.test.application.dto.request.CreateOrderRequest;
 import com.example.test.application.dto.response.CreateOrderResponse;
+import com.example.test.domain.event.OrderCreatedEvent;
 import com.example.test.domain.port.out.OrderPersist;
 import com.example.test.infra.mapper.OrderMapper;
+import com.example.test.infra.messaging.publisher.OrderEventPublisher;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -12,10 +14,13 @@ public class CreateOrderUseCase {
 
     private final OrderPersist persist;
 
+    private final OrderEventPublisher eventPublisher;
+
     private final OrderMapper mapper;
 
-    public CreateOrderUseCase(OrderPersist persist, OrderMapper mapper) {
+    public CreateOrderUseCase(OrderPersist persist, OrderEventPublisher eventPublisher, OrderMapper mapper) {
         this.persist = persist;
+        this.eventPublisher = eventPublisher;
         this.mapper = mapper;
     }
 
@@ -24,6 +29,8 @@ public class CreateOrderUseCase {
         var order = mapper.toOrder();
         order.addItems(request.items());
         persist.save(order);
+        var event = new OrderCreatedEvent(order.getId(), order.getCorrelationId(), order.getItems());
+        eventPublisher.send(event);
         return mapper.toResponse(order);
     }
 
