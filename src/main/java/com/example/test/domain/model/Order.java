ad61123c340804d3f9e152fccafbd4184ddb742e
pauloalarico <paulocraft20@gmail.com>
2026-05-31 20:19:40 -0300

    feat: add total as a logic of business of entity

--- src/main/java/com/example/test/domain/model/Order.java
+++ src/main/java/com/example/test/domain/model/Order.java
@@ -4,6 +4,7 @@ import jakarta.persistence.*;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
@@ -24,16 +25,26 @@ public class Order {
 
     private LocalDateTime createdAt;
 
+    private BigDecimal total;
+
     public static Order of() {
         Order order = new Order();
         order.id = null;
         order.correlationId = UUID.randomUUID();
         order.createdAt = LocalDateTime.now();
+        order.total = null;
         return order;
     }
 
     public void addItems(List<Item> i) {
         this.items.addAll(i);
+        calculateTotal();
+    }
+
+    private void calculateTotal() {
+        this.total = items.stream().map(
+                i -> new BigDecimal(i.getQuantity()).multiply(i.getProduct().value()))
+                .reduce(this.total != null ? total : BigDecimal.ZERO, BigDecimal::add);
     }
 
     public void addItem(Item i) {
