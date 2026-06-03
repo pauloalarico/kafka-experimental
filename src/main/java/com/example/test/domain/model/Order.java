7115640899d9252dc68f870e36c73d862c7267db
pauloalarico <paulocraft20@gmail.com>
2026-06-03 01:12:09 -0300

    fix: instance list at constructor

--- src/main/java/com/example/test/domain/model/Order.java
+++ src/main/java/com/example/test/domain/model/Order.java
@@ -6,6 +6,7 @@ import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
@@ -33,6 +34,7 @@ public class Order {
         order.correlationId = UUID.randomUUID();
         order.createdAt = LocalDateTime.now();
         order.total = null;
+        order.items = new ArrayList<>();
         return order;
     }
 
